4152142e0de31fb69a04ffef21309a3e2695e82c
chris0610 <chrislow360@gmail.com>
2022-01-22 10:07:26 -0500

    add get preset helper function in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+const getPreset = () => {
+  if (!process.env.NEXT_PUBLIC_TENANT)
+    return [require(`./tailwind/default/preset.js`)];
+
+  try {
+    return [require(`./tailwind/${process.env.NEXT_PUBLIC_TENANT}/preset.js`)];
+  } catch (err) {
+    return [require(`./tailwind/default/preset.js`)];
+  }
+};
+
 module.exports = {
+  presets: getPreset(),
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
